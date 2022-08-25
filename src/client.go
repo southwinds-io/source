4d37305a53664de258066311f744dac526239242
swskipper <skipper@southwinds.io>
2022-08-25 18:30:25 +0100

    add methods to client

--- src/client.go
+++ src/client.go
@@ -79,6 +79,74 @@ func (c *Client) SetType(key string, obj any) error {
 	return nil
 }
 
+func (c *Client) SetItem(key, itemType string, obj any) error {
+	objBytes, err := json.Marshal(obj)
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest(http.MethodPut, c.url("/item/%s", key), bytes.NewReader(objBytes))
+	if err != nil {
+		return err
+	}
+	request.Header.Set("Authorization", c.token)
+	request.Header.Set("User-Agent", fmt.Sprintf("SOURCE-CLIENT-%s", service.Version))
+	if len(itemType) > 0 {
+		request.Header.Set("Source-Type", itemType)
+	}
+	resp, reqErr := c.Do(request)
+	if reqErr != nil {
+		return reqErr
+	}
+	if resp.StatusCode > 299 {
+		return fmt.Errorf("cannot set item, source server responded with: %s", resp.Status)
+	}
+	return nil
+}
+
+func (c *Client) TagItem(key, name, value string) error {
+	var tag string
+	if len(name) > 0 {
+		if len(value) > 0 {
+			tag = fmt.Sprintf("%s|%s", name, value)
+		} else {
+			tag = name
+		}
+	} else {
+		return fmt.Errorf("a tag name is required")
+	}
+	request, err := http.NewRequest(http.MethodPut, c.url("/item/%s/tag/%s", key, tag), nil)
+	if err != nil {
+		return err
+	}
+	request.Header.Set("Authorization", c.token)
+	request.Header.Set("User-Agent", fmt.Sprintf("SOURCE-CLIENT-%s", service.Version))
+	resp, reqErr := c.Do(request)
+	if reqErr != nil {
+		return reqErr
+	}
+	if resp.StatusCode > 299 {
+		return fmt.Errorf("cannot tag item, source server responded with: %s", resp.Status)
+	}
+	return nil
+}
+
+func (c *Client) Link(fromKey, toKey string) error {
+	request, err := http.NewRequest(http.MethodPut, c.url("/link/%s/to/%s", fromKey, toKey), nil)
+	if err != nil {
+		return err
+	}
+	request.Header.Set("Authorization", c.token)
+	request.Header.Set("User-Agent", fmt.Sprintf("SOURCE-CLIENT-%s", service.Version))
+	resp, reqErr := c.Do(request)
+	if reqErr != nil {
+		return reqErr
+	}
+	if resp.StatusCode > 299 {
+		return fmt.Errorf("cannot link items, source server responded with: %s", resp.Status)
+	}
+	return nil
+}
+
 func (c *Client) url(format string, args ...any) string {
 	v := fmt.Sprintf("%s%s", c.host, fmt.Sprintf(format, args...))
 	return v
--- src/client_test.go
+++ src/client_test.go
@@ -10,9 +10,38 @@ package src
 
 import "testing"
 
-func TestSetType(t *testing.T) {
+func TestAll(t *testing.T) {
 	c := New("http://127.0.0.1:8080", "", "", nil)
+	// define a json schema for a configuration
+	// note you do not need to create the schema, it is inferred from an empty struct in this case I am using
+	// ClientOptions{}
 	err := c.SetType("AAA", ClientOptions{})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	// set a configuration: note the actual value is any object you want, in this case I am using ClientOptions{}
+	err = c.SetItem("OPT_1", "AAA", ClientOptions{
+		InsecureSkipVerify: false,
+		Timeout:            60,
+	})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	// tag the item with a name and also a value
+	err = c.TagItem("OPT_1", "status", "dev")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	// set another item
+	err = c.SetItem("OPT_2", "AAA", ClientOptions{
+		InsecureSkipVerify: true,
+		Timeout:            120,
+	})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	// associate the two items
+	err = c.Link("OPT_1", "OPT_2")
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
